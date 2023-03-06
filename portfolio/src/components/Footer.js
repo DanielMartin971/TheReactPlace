f5840300a14a2f5977ee0aeadf9ad8d126ef50cf
Daniel Martin <51744227+DanielMartin971@users.noreply.github.com>
2023-03-05 22:13:22 -0800

    added into the components for header and footer

--- portfolio/src/components/Footer.js
+++ portfolio/src/components/Footer.js
@@ -1,2 +1,19 @@
 import React from 'react';
 import LinkedInIco from '@material-ui/icons/LinkedIn';
+import GitHubIco from '@material-ui/icons/GitHub';
+import EmailIco from '@material-ui/icons/Email';
+
+function Footer() {
+    return (
+        <div className='footer'>
+            <div className='socials'>
+                <LinkedInIco onClick={event => window.location.href='https://www.linkedin.com/in/daniel-martin-6a5381188/'} />
+                <GitHubIco onClick={event => window.location.href='https://github.com/DanielMartin971'} />
+                <EmailIco onClick={event => window.location.href='mailto:danielMartin971337@gmail.com'} />
+            </div>
+            <p> &copy; 2023 All Rights Reserved</p>
+        </div>
+    )
+}
+
+export default Footer;
